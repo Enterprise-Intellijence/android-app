7c0085563c8273d3efc3225301f7312ed430b4d6
Francesco Gallo <francescogallo728@gmail.com>
2023-07-01 17:25:23 +0200

    some imporvement

--- app/src/main/java/com/enterprise/android_app/view/ProductPage.kt
+++ app/src/main/java/com/enterprise/android_app/view/ProductPage.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFrom
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PageSize
@@ -44,6 +45,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.enterprise.android_app.R
+import com.enterprise.android_app.model.components.ImageCarousell
 import kotlinx.coroutines.launch
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -72,69 +74,12 @@ fun ProductPage(){
                 )
             }) {
             contentPadding ->
-            Box(modifier = Modifier.padding(contentPadding)) {
-                val pagerState = rememberPagerState()
-                val scope = rememberCoroutineScope()
+                ImageCarousell(images = images, modifier = Modifier.padding(contentPadding))
 
-                HorizontalPager(
-                    modifier =Modifier
-                        .fillMaxWidth()
-                        .height(300.dp),
-                    pageCount = images.size,
-                    state = pagerState,
-                    key = { images[it] },
-                    pageSize = PageSize.Fill) {
-                    index ->
-                    Image(
-                        painter = painterResource(id = images[index]),
-                        contentDescription = null,
-                        contentScale = ContentScale.Crop,
-                        modifier = Modifier.fillMaxWidth() )
-
-                }
-
-
-                Box(
-                    modifier = Modifier
-                        .offset(y = -(16).dp)
-                        .fillMaxWidth(0.5f)
-                        .clip(RoundedCornerShape(100))
-                        .background(MaterialTheme.colorScheme.background)
-                        .padding(8.dp)
-                        .align(Alignment.BottomCenter)
-                ) {
-                    IconButton(
-                        onClick = {
-                            scope.launch {
-                                pagerState.animateScrollToPage(
-                                    pagerState.currentPage - 1
-                                )
-                            }
-                        },
-                        modifier = Modifier.align(Alignment.CenterStart)
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.KeyboardArrowLeft,
-                            contentDescription = "Go back"
-                        )
-                    }
-                    IconButton(
-                        onClick = {
-                            scope.launch {
-                                pagerState.animateScrollToPage(
-                                    pagerState.currentPage + 1
-                                )
-                            }
-                        },
-                        modifier = Modifier.align(Alignment.CenterEnd)
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.KeyboardArrowRight,
-                            contentDescription = "Go forward"
-                        )
-                    }
-                }
+            Column(modifier = Modifier.padding(contentPadding.calculateTopPadding())) {
+                Text(text = "dddddd")
             }
+
             }
 
         }

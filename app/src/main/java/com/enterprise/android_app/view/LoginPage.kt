f4d117721ea22370f676496aace9322b77b936b4
checcostra <strangisfrancesco2@gmail.com>
2023-07-04 18:38:49 +0200

    login fix/and registration working

--- app/src/main/java/com/enterprise/android_app/view/LoginPage.kt
+++ app/src/main/java/com/enterprise/android_app/view/LoginPage.kt
@@ -1,6 +1,5 @@
 package com.enterprise.android_app.view
 
-import android.widget.Space
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
@@ -12,6 +11,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -27,32 +27,33 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.enterprise.android_app.R
-import com.enterprise.android_app.model.CurrentDataUtils
 import com.enterprise.android_app.view.components.ButtonComponent
 import com.enterprise.android_app.view.components.ClickableLoginTextComponent
 import com.enterprise.android_app.view.components.DividerTextComponent
 import com.enterprise.android_app.view.components.HeadingTextComponent
 import com.enterprise.android_app.view.components.NormalTextComponent
-import com.enterprise.android_app.view.components.TextFieldComponent
 import com.enterprise.android_app.view.components.UnderLinedTextComponent
 import com.enterprise.android_app.navigation.AppRouter
 import com.enterprise.android_app.navigation.Screen
 import com.enterprise.android_app.ui.theme.componentShapes
-import io.swagger.client.apis.UserControllerApi
 import com.enterprise.android_app.view_models.AuthViewModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginPage(){
+
+    //only for password
+    var password by remember { mutableStateOf("")}
+    var passwordVisible by remember { mutableStateOf(false)}
 
     val authViewModel: AuthViewModel = viewModel()
     var textValueUsername by remember { mutableStateOf(TextFieldValue()) }
@@ -65,7 +66,9 @@ fun LoginPage(){
             .fillMaxSize()
             .background(Color.White)
     ) {
-        Column(modifier = Modifier.fillMaxSize().padding(28.dp)) {
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .padding(28.dp)) {
             NormalTextComponent(value = stringResource(id = R.string.hello))
 
             HeadingTextComponent(value = stringResource(id = R.string.welcome_back))
@@ -77,7 +80,7 @@ fun LoginPage(){
                 modifier = Modifier
                     .fillMaxWidth()
                     .clip(componentShapes.small),
-                label = { Text(text = "Username") },
+                label = { Text(text = stringResource(id = R.string.username)) },
 
                 colors = TextFieldDefaults.outlinedTextFieldColors(
                     focusedBorderColor = colorResource(id = R.color.colorPrimary),
@@ -103,25 +106,49 @@ fun LoginPage(){
                 modifier = Modifier
                     .fillMaxWidth()
                     .clip(componentShapes.small),
-                label = { Text(text = "Password") },
+                label = { Text(text = stringResource(id = R.string.password))},
 
                 colors = TextFieldDefaults.outlinedTextFieldColors(
                     focusedBorderColor = colorResource(id = R.color.colorPrimary),
                     focusedLabelColor =  colorResource(id = R.color.colorPrimary),
                     containerColor = colorResource(id = R.color.colorBackground),
-                    cursorColor = colorResource(id = R.color.colorPrimary),
-                    textColor = Color.Black
+                    cursorColor = colorResource(id = R.color.colorPrimary)
 
                 ),
 
-                keyboardOptions = KeyboardOptions.Default,
-                value = textValuePassword,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                value = password,
                 onValueChange = {
-                    textValuePassword = it //aggiornamento della textValue con il valore corrente
+                    password = it
                 },
+
                 leadingIcon = {
                     Icon(painterResource(id = R.drawable.password), contentDescription = "")
-                }
+                },
+
+
+                trailingIcon = {
+                    val iconImage = if (passwordVisible) {
+                        painterResource(id = R.drawable.filled_visibility)
+                        //TODO: da trovare un icona migliore
+                    } else {
+                        painterResource(id = R.drawable.filled_visibility_off)
+                        //TODO: da trovare un icona migliore
+                    }
+
+                    var description = if (passwordVisible) {
+                        stringResource(id = R.string.hide_password)
+                    } else {
+                        stringResource(id = R.string.show_password)
+                    }
+
+                    IconButton(onClick = { passwordVisible= !passwordVisible}) {
+                        Icon(painter = iconImage, contentDescription = description)
+                    }
+                },
+
+                visualTransformation = if(passwordVisible) VisualTransformation.None else
+                    PasswordVisualTransformation()
             )
 
 

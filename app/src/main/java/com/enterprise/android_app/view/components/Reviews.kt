72e4dbc4419e4d25be590eac6a494b871daedd46
ryuk4real <84407165+ryuk4real@users.noreply.github.com>
2023-07-05 12:05:12 +0200

    Added reviews component

---
+++ app/src/main/java/com/enterprise/android_app/view/components/Reviews.kt
@@ -0,0 +1,12 @@
+package com.enterprise.android_app.view.components
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Reviews() {
+    Row() {
+        Text("Reviews")
+    }
+}

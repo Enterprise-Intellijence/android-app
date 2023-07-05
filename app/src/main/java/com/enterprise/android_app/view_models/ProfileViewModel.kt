09f4d174409eeea4fad936872d6934c77d2896a7
ryuk4real <84407165+ryuk4real@users.noreply.github.com>
2023-07-05 18:42:31 +0200

    Working closet but not if it's empty

--- app/src/main/java/com/enterprise/android_app/view_models/ProfileViewModel.kt
+++ app/src/main/java/com/enterprise/android_app/view_models/ProfileViewModel.kt
@@ -1,5 +1,36 @@
 package com.enterprise.android_app.view_models
 
-class ProfileViewModel {
+import androidx.compose.runtime.mutableStateListOf
+import androidx.lifecycle.ViewModel
+import io.swagger.client.apis.ProductControllerApi
+import io.swagger.client.models.ProductBasicDTO
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
+class ProfileViewModel(visitedUserId: String) : ViewModel() {
+    private val productControllerApi: ProductControllerApi = ProductControllerApi()
+    val productList = mutableStateListOf<ProductBasicDTO>()
+    var currentPage: Int = 0
+    var visitedUserId: String = visitedUserId
+    private val coroutineScope = CoroutineScope(Dispatchers.IO)
+
+    val _productList: List<ProductBasicDTO>
+        get() = productList
+
+    fun loadNextPage() {
+        coroutineScope.launch {
+            try {
+                val newProducts = withContext(Dispatchers.IO) {
+                    productControllerApi.getFilteredProducts(userId = visitedUserId, page = currentPage)
+                }
+                val productsToAdd = newProducts.content?.toList() ?: emptyList()
+                productList.addAll(productsToAdd)
+                currentPage++
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
 }

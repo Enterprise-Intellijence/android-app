04333ee05fd57fbc91fa331c73d108253d5acef5
checcostra <strangisfrancesco2@gmail.com>
2023-07-12 12:59:48 +0200

    Controllatelo dopo grazie
    non. crusha l'applicazione, ma non mi carica l'ordine quando clicco. La navigazione è corretta ma probabilmente sono stupido io

--- app/src/main/java/com/enterprise/android_app/view/SingleOrderPage.kt
+++ app/src/main/java/com/enterprise/android_app/view/SingleOrderPage.kt
@@ -1,14 +1,36 @@
 package com.enterprise.android_app.view
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import com.enterprise.android_app.view_models.OrdersViewModel
 import io.swagger.client.models.OrderBasicDTO
 
 
 @Composable
 fun SingleOrderPage(navController: NavHostController, orderId: String){
 
+    val orderViewModel: OrdersViewModel = viewModel()
+    val order = orderViewModel.orderDTO.value
 
-    Text("$orderId")
+
+    LaunchedEffect(Unit) {
+        orderViewModel.loadSingleOrder(orderId)
+    }
+
+
+    Column() {
+        Text("Order ID: ${order?.id}")
+        Text("Order Date: ${order?.orderDate}")
+
+    }
+
+
 }
+

108b1ce3837c08e9572033c3579745f02f3a28fc
ryuk4real <84407165+ryuk4real@users.noreply.github.com>
2023-09-04 11:19:24 +0200

    Added a TextField for writing the report description

--- app/src/main/java/com/enterprise/android_app/view/ReportProductPage.kt
+++ app/src/main/java/com/enterprise/android_app/view/ReportProductPage.kt
@@ -1,10 +1,92 @@
 package com.enterprise.android_app.view
 
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import androidx.room.util.appendPlaceholders
+import com.enterprise.android_app.R
+import com.enterprise.android_app.view_models.ReportedViewModel
+import java.lang.StringBuilder
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ReportProductPage(navController: NavHostController, string: String) {
-    Text(text = "ReportProduct")
+fun ReportProductPage(navController: NavHostController, reportedProductId: String) {
+    val reportedViewModel: ReportedViewModel = viewModel()
+    var text by remember { mutableStateOf(TextFieldValue()) }
+
+    if (reportedViewModel.reportedProduct == null) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            CircularProgressIndicator(
+                modifier = Modifier.size(40.dp)
+            )
+            reportedViewModel.getProductById(reportedProductId)
+        }
+    } else {
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            TextField(
+                value = text,
+                onValueChange = {
+                    text = it
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+                    .fillMaxHeight(0.4f),
+                textStyle = TextStyle(fontSize = 18.sp),
+                singleLine = false,
+                maxLines = 10,
+                keyboardActions = KeyboardActions(
+                    onDone = {
+                        ImeAction.Done
+                    }
+                ),
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    imeAction = ImeAction.Done
+                ),
+                placeholder = { Text( text = stringResource(R.string.report_placeholder)) },
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+    }
 }
